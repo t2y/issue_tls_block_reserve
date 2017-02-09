8a4430e9cc56e48ea5dd5a38abc4c7f204daf59a
Tetsuya Morimoto <tetsuya.morimoto@gmail.com>
2017-02-09 15:46:03 +0900

    change to set server/port

--- https_client/start_clients.sh
+++ https_client/start_clients.sh
@@ -2,12 +2,15 @@
 
 CLIENT_NUM=${1:-10}
 SLEEP_SECOND=${2:-1}
+SERVER=${3:-"localhost"}
+PORT=${4:-4443}
 
 echo "start client to $CLIENT_NUM every $SLEEP_SECOND seconds"
+echo "target server/port: $SERVER:$PORT"
 
 for i in $(seq 1 $CLIENT_NUM)
 do
-    ./client_main -insecure > /dev/null 2>&1 &
+    ./client_main -server $SERVER -port $PORT -insecure > /dev/null 2>&1 &
     echo "client $i is running"
     sleep $SLEEP_SECOND
 done
